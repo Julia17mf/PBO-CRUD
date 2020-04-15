9d78c9e1929ad6d5929447612cb906594bfd1e5d
Julia17mf <53706389+Julia17mf@users.noreply.github.com>
2020-04-15 15:22:36 +0700

    Update Main.java

--- PrakCRUD/src/Main.java
+++ PrakCRUD/src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException {
-	// write your code here
-//        LihatData data = new LihatData();
-//        data.LihatData();
+	    
         new formmhs();
     }
 }
